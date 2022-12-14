a2ef494d531eca7492f5f568d5508b35876dbc3e
xEvickA <99814623+xEvickA@users.noreply.github.com>
2022-12-14 13:58:11 +0100

    Update create_script.sql

--- create_script.sql
+++ create_script.sql
@@ -2,9 +2,9 @@ create database if not exists lesson_learning;
 use lesson_learning;
 
 drop table if exists key_word_problem cascade ;
-drop table if exists category cascade ;
 drop table if exists key_word cascade ;
 drop table if exists problem cascade ;
+drop table if exists category cascade ;
 
 create table category (category_id int not null unique auto_increment primary key,
                        title varchar(225) not null unique );
@@ -18,6 +18,8 @@ create table problem(problem_id int not null unique auto_increment primary key ,
                      title varchar(225) not null,
                      description varchar(1000),
                      path varchar(225) not null,
+                     category_id int not null,
+                     FOREIGN KEY (category_id) REFERENCES category(category_id),
                      user_name varchar(100) not null,
                      create_at date not null,
                      last_editor varchar(100) deafult null,
