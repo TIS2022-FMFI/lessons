5f8a605eca7c3e2e66f037950f4a2a5f3b44dcf3
xEvickA <99814623+xEvickA@users.noreply.github.com>
2022-12-14 10:50:08 +0100

    Creation of create script
    create script of tables

--- create_script.sql
+++ create_script.sql
@@ -1,39 +1,29 @@
--- drop table if exists problem cascade;
--- create table if not exists problem
--- (
---     problem_id   serial
---     constraint lekar_pk
---     primary key,
---     meno       varchar(50) not null,
---     priezvisko varchar(50) not null
---     );
---
--- alter table lekar
---     owner to "obsivanova3@uniba.sk";
---
--- create unique index if not exists lekar_lekar_id_uindex
---     on lekar (lekar_id);
---
--- drop table if exists pacient cascade ;
--- create table if not exists pacient
--- (
---     pacient_id      serial
---     constraint pacient_pk
---     primary key,
---     rodne_cislo     serial,
---     meno            varchar(50)               not null,
---     priezvisko      varchar(50)               not null,
---     ulica           varchar(50)               not null,
---     cislo_domu      integer                   not null,
---     mesto           varchar                   not null,
---     datum_narodenia date default CURRENT_DATE not null,
---     lekar_id        integer
---     constraint pacient_lekar_fk
---     references lekar
---     );
---
--- alter table pacient
---     owner to "obsivanova3@uniba.sk";
---
--- create unique index if not exists pacient_pacient_id_uindex
---     on pacient (pacient_id);
+create database if not exists lesson_learning;
+use lesson_learning;
+
+drop table if exists key_word_problem cascade ;
+drop table if exists category cascade ;
+drop table if exists key_word cascade ;
+drop table if exists problem cascade ;
+
+create table category (category_id int not null unique auto_increment primary key,
+                       title varchar(225) not null unique );
+
+
+create table key_word(key_word_id int not null unique auto_increment primary key,
+                      title varchar(225) not null unique );
+
+create table problem(problem_id int not null unique auto_increment primary key ,
+                     title varchar(225) not null,
+                     description varchar(1000),
+                     path varchar(225) not null,
+                     user_name varchar(100) not null,
+                     create_at date not null,
+                     last_editor varchar(100),
+                     last_edited_at date);
+
+create table key_word_problem(id int not null unique auto_increment primary key,
+                              problem_id int not null,
+                              key_word_id int not null,
+                              FOREIGN KEY (problem_id) REFERENCES problem(problem_id),
+                              FOREIGN KEY (key_word_id) REFERENCES key_word(key_word_id));
