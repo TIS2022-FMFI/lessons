9691e64d6faec20ad250d3c5285b7c0015f63457
JakubMurin <murin43@uniba.sk>
2022-12-16 13:01:38 +0100

    Create lesson detail generator

---
+++ src/main/LessonDetailGenerator.java
@@ -0,0 +1,79 @@
+package main;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.Font;
+
+import main.entities.KPFinder;
+import main.entities.Key_word;
+import main.entities.Problem;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LessonDetailGenerator {
+    private static final LessonDetailGenerator INSTANCE = new LessonDetailGenerator();
+
+    public static LessonDetailGenerator getInstance() {
+        return INSTANCE;
+    }
+
+    private LessonDetailGenerator() {
+    }
+
+    public HBox lessonDetail(Problem problem) throws SQLException {
+        Text topic = new Text(75, 40, problem.getTitle());
+        topic.setStyle("-fx-font: bold 18 Calibri;");
+        topic.setTextAlignment(TextAlignment.CENTER);
+
+        List<Key_word> key_words = KPFinder.getInstance().findByProblemId(problem.getProblem_id());
+        String keywordText = "";
+        for (Key_word kw : key_words) keywordText += kw.getTitle() + ", ";
+        if (!keywordText.isEmpty()) keywordText = keywordText.substring(0, keywordText.length() - 2);
+        Text keywords = new Text(50, 70, keywordText);
+        keywords.setStyle("-fx-font: italic 16 Calibri;");
+        keywords.setTextAlignment(TextAlignment.CENTER);
+
+        VBox info = new VBox(topic, keywords);
+        info.setMinSize(500, 100);
+
+        Image image = new Image("lupa3.png");
+
+        ImageView firstImage = new ImageView(image);
+        firstImage.setFitHeight(150);
+        firstImage.setFitWidth(150);
+        firstImage.setPickOnBounds(true);
+        firstImage.setPreserveRatio(true);
+        HBox.setMargin(firstImage, new Insets(15));
+
+        ImageView secondImage = new ImageView(image);
+        secondImage.setFitHeight(150);
+        secondImage.setFitWidth(150);
+        secondImage.setPickOnBounds(true);
+        secondImage.setPreserveRatio(true);
+        HBox.setMargin(secondImage, new Insets(10));
+
+
+        Button show = new Button("SHOW");
+        show.setFont(Font.font("Calibri", FontWeight.BOLD, 20));
+        show.setTextAlignment(TextAlignment.CENTER);
+        HBox.setMargin(show, new Insets(50, 10, 50, 10));
+
+        Button modal = new Button("MODAL");
+        modal.setFont(Font.font("Calibri", FontWeight.BOLD, 20));
+        modal.setTextAlignment(TextAlignment.CENTER);
+        HBox.setMargin(modal, new Insets(50, 10, 50, 10));
+
+        HBox detail = new HBox(info, firstImage, secondImage, show, modal);
+        detail.setMinSize(1000, 150);
+        return detail;
+    }
+}
+
